c32b732d3b685ccfaf5e16f6ef54292d8c173afc
Paul Burdick <paul@reedmaniac.com>
2019-01-02 10:03:52 -0800

    Updating trait name for Groot Element

--- src/Templates/Elements/Groot.php
+++ src/Templates/Elements/Groot.php
@@ -2,10 +2,10 @@
 
 namespace Kilvin\Plugins\Groot\Templates\Elements;
 
-use Kilvin\Libraries\Twig\Templates\Element as TemplateElement;
+use Kilvin\Libraries\Twig\Templates\ModelElement;
 use Kilvin\Plugins\Groot\Models\Groot as BaseModel;
 
 class Groot extends BaseModel implements \IteratorAggregate
 {
-    use TemplateElement;
+    use ModelElement;
 }
